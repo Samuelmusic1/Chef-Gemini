3d5c0e0c9f5e63d45b2ae0f61d29e97551b5e944
Samuel Babalola <98273940+Samuelmusic1@users.noreply.github.com>
2026-04-22 01:01:46 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -4,7 +4,7 @@ const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
 `
 const openAI = new OpenAI({
-    apiKey: process.env.ANTHROPIC_API_KEY,
+    apiKey: import.meta.env.VITE_AI_KEY,
     baseURL: "https://openrouter.ai/api/v1",
     dangerouslyAllowBrowser: true
 })
