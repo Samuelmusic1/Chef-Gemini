c673403dfd350acf254371d03c35b50f338cb224
Samuel Babalola <98273940+Samuelmusic1@users.noreply.github.com>
2026-05-27 18:23:34 +0100

    Update ai.js

--- ai.js
+++ ai.js
@@ -37,7 +37,7 @@ export async function getRecipeFromChefGemini(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ")
 
     const response = await openAI.responses.create({
-        model: "google/gemma-4-31b-it:free",
+        model: "openrouter/owl-alpha",
         max_tokens: 1024,
         input: [
             { role: "system", content: SYSTEM_PROMPT },
